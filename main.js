6c98b3ea54b59a5cfb00748aa13e0839340bf3d7
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-12-28 15:45:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristX = 0;
 
+scoreRightWrist =0;
+scoreLeftWrist = 0;
+
+song1_status ="";
+song2_status = "";
 
 
 function preload(){
@@ -26,6 +31,33 @@ function setup(){
 
 function draw(){
     image(video , 0 , 0 , 600 , 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#ff0000");
+    stroke("#ff0000");
+
+    if(scoreRightWrist < 0.2){
+        circle(rightWristX , rightWristY , 20);
+        song2.stop();
+
+        if(song1_status == false){
+            song1.play();
+            document.getElementById("song").innerHTML = "playing harry potter song";
+        }
+    }
+
+    if(scoreLeftyWrist < 0.2){
+        circle(leftWristX , leftWristY , 20);
+        song1.stop();
+
+        if(song2_status == false){
+            song2.play();
+            document.getElementById("song").innerHTML = "playing peter pan song";
+        }
+    }
+
 }
 
 
@@ -40,14 +72,18 @@ function gotPoses(results){
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("left wrist x " + leftWristX + " left wrist y " + leftWristY);
+        scoreLeftWrist = results[0].pose.keyPoints[0].score;
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
 
         console.log("right wrist x " + rightWristX + " right wrist y " + rightWristY);
+        scoreRightWrist = results[0].pose.keyPoints[10].score;
 
 
 
 
     }
+
+    
 }
